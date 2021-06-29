faaaf1f10536b85de2bf373708b9ddc0996fcae0
jeromeraphael <83524408+jeromeraphael@users.noreply.github.com>
2021-06-28 17:56:59 -0700

    Project Setup

---
+++ portfolio_website/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+// MAIN RUNNING CODE
+
+
+function App() {
+  return (
+    <h2>Portfolio Project</h2>
+      
+  );
+}
+
+export default App;
